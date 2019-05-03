1e6aae792d02752ced44029fffc3d1f29a3f3c42
mistborn6 <steven96339@gmail.com>
2019-05-03 23:30:23 +0800

    complete hw7

---
+++ answers/hw7/js/lang-en.js
@@ -0,0 +1,5 @@
+if (!window.I18N) window.I18N = {};
+window.I18N['en'] = {
+  TITLE: 'Streams in English',
+}
+
---
+++ answers/hw7/js/lang-zh.js
@@ -0,0 +1,5 @@
+if (!window.I18N) window.I18N = {};
+window.I18N['zh'] = {
+  TITLE: '用中文直播的頻道',
+}
+
